7a6fcbb39d221fdc4b6dd5f928a30aa6487e35b2
Yash Rajesh Sali <161243429+YASHSALI2005@users.noreply.github.com>
2025-04-19 23:27:23 +0530

    vite.config.js

--- Frontend/travel/vite.config.js
+++ Frontend/travel/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'Frontend/travel', // Set the root to the travel folder
   plugins: [react()],
+  build: {
+    outDir: '../../dist', // Output the build in a parent directory (outside the travel folder)
+  },
 })
