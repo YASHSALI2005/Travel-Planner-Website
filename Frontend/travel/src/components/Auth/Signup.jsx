b49da56fc35ba3f49fb3b936cd91f270add14cdd
Yash Rajesh Sali <161243429+YASHSALI2005@users.noreply.github.com>
2025-06-10 22:57:52 +0530

    Signup.jsx

--- Frontend/travel/src/components/Auth/Signup.jsx
+++ Frontend/travel/src/components/Auth/Signup.jsx
@@ -29,7 +29,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await axios.post('/api/signup', {
+      const response = await axios.post('${import.meta.env.VITE_BACKEND_URL}/api/signup', {
         email,
         password,
         confirmPassword
